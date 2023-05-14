c41adfd3f5485ea8b2959626119c6d91940444a2
ezratweaver <ezratweaver@gmail.com>
2023-05-13 21:37:59 -0500

    Boilerplate Code

---
+++ window.py
@@ -0,0 +1,20 @@
+from tkinter import Tk, Canvas
+
+WINDOW_COLOR = "#3556FB"
+window = Tk()
+
+
+class Background:
+
+    def __init__(self):
+        background = Canvas(window, bg=WINDOW_COLOR,
+                            height=500, width = 800,
+                            bd=0, highlightthickness=0,
+                            relief="ridge")
+        background.pack()
+
+window.geometry("800x500")
+window.configure(bg=WINDOW_COLOR)
+window.title("Tkinter Math Flash Cards")
+window.resizable(False, False)
+window.mainloop()
