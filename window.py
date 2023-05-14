98b3a72195c831ac9ff5bbd192791af6705d94d7
ezratweaver <ezratweaver@gmail.com>
2023-05-14 17:50:00 -0500

    Circle Moves

--- window.py
+++ window.py
@@ -1,19 +1,34 @@
-from tkinter import Tk, Canvas
-from assets import Assets
-
-
-WINDOW_COLOR = "#3556FB"
-window = Tk()
-
+from tkinter import Canvas, Label
+from assets import Assets, window, WINDOW_COLOR
 
 class Background:
 
     def __init__(self):
-        background = Canvas(window, bg=WINDOW_COLOR,
+        self.background = Canvas(window, bg=WINDOW_COLOR,
                             height=500, width = 800,
                             bd=0, highlightthickness=0,
                             relief="ridge")
-        background.pack()
+        self.background.pack()
+
+        self.circle1 = self.background.create_image(
+            100,
+            100,
+            image=Assets.image_circle1
+        )
+
+        print(self.background.coords(self.circle1))
+
+    def move_circle1(self):
+        current_cordinates = self.background.coords(self.circle1)
+
+        new_cords = [current_cordinates[0] + 3, current_cordinates[1]]
+
+        self.background.coords(self.circle1, *new_cords)
+
+        self.background.after(10, self.move_circle1)
+
+bg = Background()
+bg.move_circle1()
 
 window.geometry("800x500")
 window.configure(bg=WINDOW_COLOR)
