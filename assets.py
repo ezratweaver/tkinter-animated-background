bf6bad95914d61027cff98953d8b322fe1707ba1
ezratweaver <ezratweaver@gmail.com>
2023-05-14 17:50:06 -0500

    Added Circle

--- assets.py
+++ assets.py
@@ -1,10 +1,12 @@
-from tkinter import PhotoImage
+from tkinter import PhotoImage, Tk
 from os import path, chdir
 from sys import argv
 
 EXE_DIR = path.dirname(argv[0])
 chdir(EXE_DIR)
 
+WINDOW_COLOR = "#3556FB"
+window = Tk()
 
 class Assets():
-    pass
+    image_circle1 = PhotoImage(file="Assets/circle1.png")
