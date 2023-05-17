368b96c1a40c75129b193992d7d3e20e07961c03
ezratweaver <ezratweaver@gmail.com>
2023-05-17 18:52:11 -0500

    Added New Assets Into Code

--- assets.py
+++ assets.py
@@ -10,4 +10,13 @@ window = Tk()
 
 class Assets():
     image_circle1 = PhotoImage(file="Assets/circle1.png")
+    image_circle2 = PhotoImage(file="Assets/circle2.png")
+    image_circle3 = PhotoImage(file="Assets/circle3.png")
+    image_circle4 = PhotoImage(file="Assets/circle4.png")
     image_square1 = PhotoImage(file="Assets/square1.png")
+    image_square2 = PhotoImage(file="Assets/square2.png")
+    image_square3 = PhotoImage(file="Assets/square3.png")
+    image_star1 = PhotoImage(file="Assets/star1.png")
+    image_star2 = PhotoImage(file="Assets/star2.png")
+    image_star3 = PhotoImage(file="Assets/star3.png")
+    image_triangle1 = PhotoImage(file="Assets/triangle1.png")
