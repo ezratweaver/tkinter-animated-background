93cd977b38786636f7a89753bfc8dd338e7a5659
ezratweaver <ezratweaver@gmail.com>
2023-05-18 11:00:27 -0500

    Better Randomness | W.I.P Collision

--- window.py
+++ window.py
@@ -1,6 +1,6 @@
 from tkinter import Canvas
 from assets import asset_manifest, window, WINDOW_COLOR
-from random import randint
+from random import randint, choice
 
 class Background:
 
@@ -30,15 +30,25 @@ class Background:
 
         self.background.coords(object, *new_cords)
 
-
-        if current_cordinates[0] < start_x:
+        if Background.is_near(current_cordinates[0], start_x, 3):
             x_velocity = abs(x_velocity)
-        if current_cordinates[0] > end_x:
+            print(f"Current Cordinates: {current_cordinates[0]}  Velocity: {x_velocity}  Trigger: {start_x}  Axis: X  Point: Start")
+        if Background.is_near(current_cordinates[0], end_x, 3):
             x_velocity = -abs(x_velocity)
-        if current_cordinates[1] < start_y:
+            print(f"Current Cordinates: {current_cordinates[0]}  Velocity: {x_velocity}  Trigger: {end_x}  Axis: X  Point: End")
+        if Background.is_near(current_cordinates[1], start_y, 3):
             y_velocity = abs(y_velocity)
-        if current_cordinates[1] > end_y:
+            print(f"Current Cordinates: {current_cordinates[1]}  Velocity: {y_velocity}  Trigger: {start_y}  Axis: Y  Point: Start")
+        if Background.is_near(current_cordinates[1], end_y, 3):
             y_velocity = -abs(y_velocity)
+            print(f"Current Cordinates: {current_cordinates[1]}  Velocity: {y_velocity}  Trigger: {end_y}  Axis: Y  Point: End")
+
+        # for alien_object in self.placed_images:
+        #     alien_coords = self.background.coords(alien_object)
+        #     if Background.is_near(current_cordinates[0], alien_coords[0], 3):
+        #         x_velocity = abs(x_velocity)
+        #     if Background.is_near(current_cordinates[1], alien_coords[1], 3):
+        #         x_velocity = abs(y_velocity)
 
 
         self.background.after(20, self.move_object,
@@ -47,7 +57,7 @@ class Background:
                                         start_y, end_y)
     
     @staticmethod
-    def is_near(self, number, target, tolerance):
+    def is_near(number, target, tolerance):
         """
         Checks if a number is near another number within a given tolerance.
         
@@ -63,11 +73,12 @@ class Background:
         return abs(number - target) <= tolerance
 
 
-move_object_args = [3, 3, 0, 800, 0, 500]
+move_object_args = [randint(-3, 3), randint(-3, 3), 0, 800, 0, 500]
 bg = Background()
 
+random_velocity = [-3, -2, -1, 1, 2, 3]
 for placed_image in bg.placed_images:
-    bg.move_object(placed_image, *move_object_args)
+    bg.move_object(placed_image, choice(random_velocity), choice(random_velocity), 0, 800, 0, 500)
 
 window.geometry("800x500")
 window.title("Wallpaper")
