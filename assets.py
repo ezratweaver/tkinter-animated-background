b0c2fe6662641835405125f7af870697aa0be3b9
ezratweaver <ezratweaver@gmail.com>
2023-05-18 11:07:45 -0500

    Simplifying Assets

--- assets.py
+++ assets.py
@@ -1,6 +1,7 @@
 from tkinter import PhotoImage, Tk
 from os import path, chdir
 from sys import argv
+from itertools import chain
 
 EXE_DIR = path.dirname(argv[0])
 chdir(EXE_DIR)
@@ -9,17 +10,11 @@ WINDOW_COLOR = "#3556FB"
 window = Tk()
 
 class Assets():
-    image_circle1 = PhotoImage(file="Assets/circle1.png")
-    image_circle2 = PhotoImage(file="Assets/circle2.png")
-    image_circle3 = PhotoImage(file="Assets/circle3.png")
-    image_circle4 = PhotoImage(file="Assets/circle4.png")
-    image_square1 = PhotoImage(file="Assets/square1.png")
-    image_square2 = PhotoImage(file="Assets/square2.png")
-    image_square3 = PhotoImage(file="Assets/square3.png")
-    image_star1 = PhotoImage(file="Assets/star1.png")
-    image_star2 = PhotoImage(file="Assets/star2.png")
-    image_star3 = PhotoImage(file="Assets/star3.png")
-    image_triangle1 = PhotoImage(file="Assets/triangle1.png")
+    image_circle = PhotoImage(file="Assets/circle.png")
+    image_square = PhotoImage(file="Assets/square.png")
+    image_star = PhotoImage(file="Assets/star.png")
+    image_triangle = PhotoImage(file="Assets/triangle.png")
 
 
 asset_manifest = [getattr(Assets, x) for x in dir(Assets) if x.startswith("image")]
+asset_manifest = list(chain(asset_manifest * 7))
