6ae8119df35063ee562531d3ebb1090235ef94d2
ezratweaver <ezratweaver@gmail.com>
2023-05-22 20:28:43 -0500

    Moving Animation Function Into New Class

--- window.py
+++ window.py
@@ -19,35 +19,40 @@ class Background:
                 image=image
             ))
 
-    def move_object(self, object,
-                    x_velocity, y_velocity,
-                    start_x, end_x,
-                    start_y, end_y):
-        current_cordinates = self.background.coords(object)
+class BackgroundObject:
 
-        new_cords = [current_cordinates[0] + x_velocity, 
-                    current_cordinates[1] + y_velocity]
+    def __init__(self, object, start_x, end_x, start_y, end_y):
+        random_velocity = [-2, -1, 1, 2]
+        self.object = object
+        self.x_velocity = choice(random_velocity)
+        self.y_velocity = choice(random_velocity)
+        self.start_x = start_x
+        self.end_x = end_x
+        self.start_y = start_y
+        self.end_y = end_y
 
-        self.background.coords(object, *new_cords)
+    def start_animation(self, *args):
+        current_cordinates = bg.background.coords(self.object)
 
-        if Background.is_near(current_cordinates[0], start_x, 3):
-            x_velocity = abs(x_velocity)
-            print(f"Current Cordinates: {current_cordinates[0]}  Velocity: {x_velocity}  Trigger: {start_x}  Axis: X  Point: Start")
-        if Background.is_near(current_cordinates[0], end_x, 3):
-            x_velocity = -abs(x_velocity)
-            print(f"Current Cordinates: {current_cordinates[0]}  Velocity: {x_velocity}  Trigger: {end_x}  Axis: X  Point: End")
-        if Background.is_near(current_cordinates[1], start_y, 3):
-            y_velocity = abs(y_velocity)
-            print(f"Current Cordinates: {current_cordinates[1]}  Velocity: {y_velocity}  Trigger: {start_y}  Axis: Y  Point: Start")
-        if Background.is_near(current_cordinates[1], end_y, 3):
-            y_velocity = -abs(y_velocity)
-            print(f"Current Cordinates: {current_cordinates[1]}  Velocity: {y_velocity}  Trigger: {end_y}  Axis: Y  Point: End")
+        new_cords = [current_cordinates[0] + self.x_velocity, 
+                    current_cordinates[1] + self.y_velocity]
 
-        self.background.after(20, self.move_object,
-                                        object, x_velocity, 
-                                        y_velocity, start_x, end_x,
-                                        start_y, end_y)
-    
+        bg.background.coords(self.object, *new_cords)
+
+        if BackgroundObject.is_near(current_cordinates[0], self.start_x, 3):
+            self.x_velocity = abs(self.x_velocity)
+        if BackgroundObject.is_near(current_cordinates[0], self.end_x, 3):
+            self.x_velocity = -abs(self.x_velocity)
+        if BackgroundObject.is_near(current_cordinates[1], self.start_y, 3):
+            self.y_velocity = abs(self.y_velocity)
+        if BackgroundObject.is_near(current_cordinates[1], self.end_y, 3):
+            self.y_velocity = -abs(self.y_velocity)
+
+        bg.background.after(20, self.start_animation,
+                                        object, self.x_velocity, 
+                                        self.y_velocity, self.start_x, self.end_x,
+                                        self.start_y, self.end_y)
+        
     @staticmethod
     def is_near(number, target, tolerance):
         """
@@ -65,15 +70,13 @@ class Background:
         return abs(number - target) <= tolerance
 
 
-move_object_args = [randint(-3, 3), randint(-3, 3), 0, 800, 0, 500]
 bg = Background()
 
-random_velocity = [-2, -1, 1, 2]
+moving_objects = []
 for placed_image in bg.placed_images:
-    bg.move_object(
-        placed_image, choice(random_velocity), 
-        choice(random_velocity), 0, 800, 
-        0, 500)
+    moving_objects.append(BackgroundObject(placed_image, 0, 800, 0, 500))
+for object in moving_objects:
+    object.start_animation()
 
 window.geometry("800x500")
 window.title("Wallpaper")
